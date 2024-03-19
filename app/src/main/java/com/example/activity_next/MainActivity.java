0ccf7d47700a6eb502d390c075dbaa7211fceccc
AidaBosri <aidabosri4@gmail.com>
2024-03-19 12:18:18 +0800

    First commit

---
+++ app/src/main/java/com/example/activity_next/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.activity_next;
+
+import android.annotation.SuppressLint;
+import android.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.view;
+import android.widget.Button;
+import android.os.Bundle;
+public class NextActivity extends AppCompatActivity {
+
+    Button button;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        button = findViewById(R.id.bu)
+        });
+    }
+}
